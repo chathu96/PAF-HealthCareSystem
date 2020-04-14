461675024ca84da54573769cd4eaee8e0c8d0835
TechWorld-2020 <chathuryaatthanayake2015@gmail.com>
2020-04-14 18:34:20 +0530

    UpdatedAPI

--- Doctor/src/com/doctor_Service.java
+++ Doctor/src/com/doctor_Service.java
@@ -42,42 +42,4 @@ public class doctor_Service {
 		return output; 
 	}
 	
-	@PUT 
-	@Path("/") 
-	@Consumes(MediaType.APPLICATION_JSON) 
-	@Produces(MediaType.TEXT_PLAIN) 
-	public String updateDoctors(String DoctorsData) { 
-		//Convert the input string to a JSON object  
-		JsonObject doctorObject = new JsonParser().parse(DoctorsData).getAsJsonObject(); 
-		 
-		 //Read the values from the JSON object  
-		String Doctor_ID = doctorObject.get("Doctor_ID").getAsString();  
-		String Doctor_Name = doctorObject.get("Doctor_Name").getAsString();  
-		String Doctor_Specialization = doctorObject.get("Doctor_Specialization").getAsString();  
-		String Doctor_MedicalRegistrationNo = doctorObject.get("Doctor_MedicalRegistrationNo").getAsString();  
-		String Doctor_ContactNo = doctorObject.get("Doctor_ContactNo").getAsString();
-		String Doctor_Address = doctorObject.get("Doctor_Address").getAsString();   
-		String Doctor_Email = doctorObject.get("Doctor_Email").getAsString(); 
-		String Doctor_NIC = doctorObject.get("Doctor_NIC").getAsString(); 
-		 
-		String output = doctorObj.updateDoctors(Doctor_ID, Doctor_Name, Doctor_Specialization, Doctor_MedicalRegistrationNo, Doctor_ContactNo, Doctor_Address, Doctor_Email, Doctor_NIC); 
-		 
-		return output; 
-	}
-	
-	@DELETE 
-	@Path("/") 
-	@Consumes(MediaType.APPLICATION_XML) 
-	@Produces(MediaType.TEXT_PLAIN) 
-	public String deleteDoctors(String DoctorsData) {  
-		//Convert the input string to an XML document  
-		Document doc = Jsoup.parse(DoctorsData, "", Parser.xmlParser());     
-		
-		//Read the value from the element <Hospital_ID>  
-		String Doctor_ID = doc.select("Doctor_ID").text(); 
-		 
-		 String output = doctorObj.deleteDoctors(Doctor_ID); 
-		 
-		 return output; 
-		 }
 }
