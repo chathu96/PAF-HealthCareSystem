03e557ef090dcce03be22b831f9281de6df66034
TechWorld-2020 <chathuryaatthanayake2015@gmail.com>
2020-04-18 03:27:17 +0530

    AddignDtoPackage

---
+++ Doctor/src/main/java/com/healthcare/doctorapi/dto/DoctorAddService.java
@@ -0,0 +1,14 @@
+package com.healthcare.doctorapi.dto;
+
+public class DoctorAddService {
+
+	public DoctorAddService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Doctor/src/main/java/com/healthcare/doctorapi/dto/UpdateDoctor.java
@@ -0,0 +1,64 @@
+package com.healthcare.doctorapi.dto;
+
+public class UpdateDoctor {
+	private Integer Doctor_ID;
+	private String Doctor_Name;
+	private String Doctor_Specialization;
+	private String Doctor_MedicalRegistrationNo;
+	private String Doctor_ContactNo;
+	private String Doctor_Address;
+	private String Doctor_Email;
+	private String Doctor_NIC;
+	public UpdateDoctor() {
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getDoctor_ID() {
+		return Doctor_ID;
+	}
+	public void setDoctor_ID(Integer doctor_ID) {
+		Doctor_ID = doctor_ID;
+	}
+	public String getDoctor_Name() {
+		return Doctor_Name;
+	}
+	public void setDoctor_Name(String doctor_Name) {
+		Doctor_Name = doctor_Name;
+	}
+	public String getDoctor_Specialization() {
+		return Doctor_Specialization;
+	}
+	public void setDoctor_Specialization(String doctor_Specialization) {
+		Doctor_Specialization = doctor_Specialization;
+	}
+	public String getDoctor_MedicalRegistrationNo() {
+		return Doctor_MedicalRegistrationNo;
+	}
+	public void setDoctor_MedicalRegistrationNo(String doctor_MedicalRegistrationNo) {
+		Doctor_MedicalRegistrationNo = doctor_MedicalRegistrationNo;
+	}
+	public String getDoctor_ContactNo() {
+		return Doctor_ContactNo;
+	}
+	public void setDoctor_ContactNo(String doctor_ContactNo) {
+		Doctor_ContactNo = doctor_ContactNo;
+	}
+	public String getDoctor_Address() {
+		return Doctor_Address;
+	}
+	public void setDoctor_Address(String doctor_Address) {
+		Doctor_Address = doctor_Address;
+	}
+	public String getDoctor_Email() {
+		return Doctor_Email;
+	}
+	public void setDoctor_Email(String doctor_Email) {
+		Doctor_Email = doctor_Email;
+	}
+	public String getDoctor_NIC() {
+		return Doctor_NIC;
+	}
+	public void setDoctor_NIC(String doctor_NIC) {
+		Doctor_NIC = doctor_NIC;
+	}
+
+}
